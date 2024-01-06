380306e29d9d6fca8bd5fad2c855fa86c04b8b26
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-01-06 22:42:54 +0000

    fix stack smashing

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
   * _strncat - concanate two strings
   *@dest: 1st string
@@ -5,20 +6,19 @@
   *@n: whre to trancate the string
   *Return: pointer to the new string
   */
-#include "main.h"
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int len, j;
-	char lc;
+	int len, j = 0;
+
 
 	len = 0;
-	lc = src[n];
 
-	while (dest[len] != '\0')
-		++len;
 
-	for (j = 0; src[j] < lc; ++j, ++len)
-		dest[len] = src[j];
+	while (dest[j++])
+		len++;
+	for (j = 0; src[j] && j < n; j++)
+		dest[len++] = *(src + j);
 	dest[len] = '\0';
 	return (dest);
 }
