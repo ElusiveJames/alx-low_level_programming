a1a61605cfb0836480e9bf66d518aff3c2935901
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-01-07 21:02:45 +0000

    never reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+/**
+  *reverse_array - reverses element in an array;
+  *@a: pointer to array of elements
+  *@n: number of elements
+  */
+#include "main.h"
+void reverse_array(int *a, int n)
+{
+	int i, x;
+
+	for (i = (n - 1); i > n / 2; i--)
+	{
+		x = a[i];
+		[i] = a[n - 1 - i];
+		[n - 1 - i] = x;
+	}
+}
