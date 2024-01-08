513bde43b2d0bd3080319f39ab4cc47c3ff48152
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-01-08 01:35:22 +0000

    prepare for the wworst

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
+char *cap_string(char *);
 char *string_toupper(char *);
 #endif
