ec2b9a22cd29e5ab2e5bde67ff465930fbd23c43
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-01-15 01:57:01 +0000

    goodness of God

--- 0-puts_recursion.c
+++ 0-puts_recursion.c
@@ -6,12 +6,12 @@
 void _puts_recursion(char *s)
 {
 	int i = 0;
-	
+
 	if (*s == 0)
 	{
 		_putchar('\n');
 		return;
 	}
 	_putchar(s[i]);
-	_puts_recursion( s + 1);
-}	
+	_puts_recursion(s + 1);
+}
