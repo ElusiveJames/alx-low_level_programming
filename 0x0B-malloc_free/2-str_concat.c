b71ed5e9315ad08c30a5ec362830dfe2de02abf3
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-01-17 01:09:39 +0000

    handle null string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,11 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k, m;
 	char *str;
 	int  t_len;
-
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 =  "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
