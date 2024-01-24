8b76c4c8c00737bd58d126ebf6268d112a945455
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-01-24 06:41:53 +0000

    using stdlib to print file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
   *main - print name of source code
   *Return: zero if success
   */
-#include <stdio.h>
+#include <stdlib.h>
+#include<stdio.h>
 #include "main.h"
 int  main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
-
 }
