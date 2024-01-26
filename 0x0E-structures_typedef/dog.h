4bc98a28bd87ecd78b62c431674c043a544774d4
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-01-26 04:22:42 +0000

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+  *struct dog - structure of a dog
+  *@name: name of the dog
+  *@age: age parameter
+  *@owner: who own the dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
