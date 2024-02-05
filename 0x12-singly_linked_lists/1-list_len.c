0a0b7be4ecfc598dbec96f0da1f6459fc0cd85b6
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-02-05 16:55:13 +0000

    List lenght

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,17 +4,15 @@
   *Return: no of elements in the list
   */
 #include "lists.h"
-#include <stdio.h>
 
 size_t list_len(const list_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
-	if (h == NULL)
-		printf("list is empty");
 	while (h  != NULL)
 	{
 		count++;
 		h = h->next;
 	}
 	return (count);
+}
