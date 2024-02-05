18a352920ddc4dc5c7ba04e46cad786644b2def7
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-02-05 15:10:08 +0000

    add modules into single header file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,24 +4,21 @@
   *Return: no of nodes
   */
 #include "lists.h"
-#include <stdio.h>
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 	const list_t *ptr;
 
 	ptr = h;
-	if (ptr == NULL)
-		printf("list is empty");
 	while (ptr != NULL)
 	{
 		count++;
 		if (ptr->str == NULL)
-			printf("[0] (nil)\n");
-		printf("[%d] %s\n ", ptr->len, ptr->str);
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n ", ptr->len, ptr->str);
 		ptr = ptr->next;
 	}
-	ptr = h;
 	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,10 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef _LISTS_
+#define _LISTS_
 #include "structure.h"
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
