89f9ca43731e4153a49c6fa5d943c29f166a3a28
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-02-05 22:40:56 +0000

    freelist

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,18 +2,17 @@
  *free_list - free list in memeory
  *@head: pointer to list header
  */
-
 #include "lists.h"
+
+
 void free_list(list_t *head)
 {
+	list_t *current;
 
-	list_t *ptr = head;
-
-	while (ptr != NULL)
+	while ((current = head) != NULL)
 	{
-		head  = head->next;
-		free(ptr->str);
-		free(ptr);
+		head = head->next;
+		free(current->str);
+		free(current);
 	}
 }
-
