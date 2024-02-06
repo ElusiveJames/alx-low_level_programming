83b96ca4577229961a2e0f585c7bc81e184fe1e8
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-02-06 22:44:37 +0000

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+/**
+  *free_listint2 - free a list
+  *@head: pointer to list
+  */
+#include "lists.h"
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while ((ptr = *head) != NULL)
+	{
+		ptr = ptr->next;
+		free(*head);
+		*head = ptr;
+	}
+
+}
