c55146793c98aff59beaa9173afa9126271c9d29
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-02-08 10:32:54 +0000

    still inserting

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,22 +14,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	ptr = *head;
 	temp = malloc(sizeof(listint_t));
-	if (temp == NULL)
-	{
+	if (temp || !head)
 		return (NULL);
-	}
+
 	temp->n = n;
 	temp->next = NULL;
-	if (*head == NULL)
-	{
-		return (NULL);
-	}
+
 	if (idx == 0)
 	{
 		temp->next = *head;
-		*head =  temp;
+		*head = temp;
 		return (temp);
 	}
+
+
 	for (x = 0; ptr && x < idx; x++)
 	{
 		if (x == idx - 1)
