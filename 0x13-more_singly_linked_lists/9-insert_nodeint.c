d727749347fd92e6ec065da5825ea1775188ea1c
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-02-08 09:53:41 +0000

    insert at beginning

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,6 +30,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			ptr = ptr->next;
 			x++;
+			if (ptr == NULL)
+				return (NULL);
+		}
+		if (x <= 0)
+		{
+			temp->next = ptr;
+			ptr = temp;
 		}
 		if (ptr == NULL)
 		{
