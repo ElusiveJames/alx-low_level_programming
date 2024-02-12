e3c223761b17e617643521f81fcbd7f07c95c385
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-02-12 10:59:26 +0000

    11

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+/**
+  *set_bit - set bit value to 1
+  *@n: number
+  *@index: position
+  *Return: 1 if success else -1
+  */
+#include "main.h"
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int mask;
+
+	if (index > (sizeof(unsigned int) * 8) - 1 || n  == NULL)
+		return (-1);
+	mask = 1 << index;
+	*n = *n | mask;
+	return (1);
+}
