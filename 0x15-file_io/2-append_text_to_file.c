bc172f9e7a887d07c559c596483e20ef79f37bdb
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-02-23 21:30:22 +0000

    Speak gently,she can be near

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,36 +1,46 @@
-#include "main.h"
-#include <unistd.h>
 /**
-  *append_text_to_file - appending
-  *@filename: file to use
-  *@text_content: string to  apppend
+  *append_text_to_file - append  to  file
+  *@filename:  this file
+  *@text_content: string to insert to file
   *Return: 1 on success
   */
 #include "main.h"
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd;
 	int count = 0;
-	int output = 0;
-	int fd = 0;
+	int re_write;
 
-	if (filename != NULL)
-		fd = open(filename, O_WRONLY | O_APPEND);
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 	else
+	{
+		fd = open(filename, O_WRONLY | O_APPEND);
+		if (fd == -1)
+		{
 		return (-1);
-	if (fd == -1)
-		return (-1);
-
+		}
+	}
 	if (text_content != NULL)
 	{
 		while (text_content[count] != '\0')
+		{
 			count++;
+		}
 	}
 	else
+	{
+		close(fd);
 		return (1);
-
-	output = write(fd, text_content, count);
-	if (output == -1)
+	}
+	re_write = write(fd, text_content, count);
+	if (re_write == -1)
+	{
 		return (-1);
+	}
 	close(fd);
 	return (1);
 }
+
