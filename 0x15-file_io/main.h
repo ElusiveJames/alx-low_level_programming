f10f51b4abcb354fa1453fc3cd62d8dfe68b45b8
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-02-23 17:55:48 +0000

    Tread lightly she is near

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+
+#endif
