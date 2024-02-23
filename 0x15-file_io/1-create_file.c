f0ab0ffbac469d50d639a3e8b4b1bff7ac8ff3f1
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-02-23 20:52:48 +0000

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,44 @@
 /**
-  *create file -  create filet
+  *create_file -  create  a new file if != existt
   *@filename:  this file
-  *@text_content: this string
+  *@text_content: string to insert to file
   *Return: 1 on success
   */
 #include "main.h"
- int create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int count = 0;
-	int out = 0;
+	int re_write;
 
 	if (filename == NULL)
+	{
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNK, 0600);
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
+	{
 		return (-1);
+	}
 	if (text_content != NULL)
 	{
 		while (text_content[count] != '\0')
+		{
 			count++;
+		}
 	}
 	else
 	{
 		close(fd);
 		return (1);
 	}
-	output = write(fd, text_content, count);
-	if (output == -1 || output != count)
-		return (-1);
 
+	re_write = write(fd, text_content, count);
+	if (re_write == -1 || count != re_write)
+	{
+		return (-1);
+	}
 	close(fd);
 	return (1);
 }
