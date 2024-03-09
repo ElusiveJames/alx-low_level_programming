0df5c1fe1574f6ef7d5f3ffce6027a6532fb736e
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-03-09 18:31:22 +0000

    all is well

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,6 +4,7 @@
 #include "main.h"
 void times_table(void)
 {
+
 	int row, col, prod;
 
 	for (row = 0; row < 10; row++)
@@ -11,16 +12,21 @@ void times_table(void)
 		for (col = 0; col < 10; col++)
 		{
 			prod = row * col;
+
 			if (prod > 9)
 			{
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
 			}
-			else
+			if (col > 0 && prod < 10) 
 			{
 				_putchar(32);
 				_putchar(prod + '0');
+
 			}
+			if (col < 1 && prod < 10)
+				_putchar(prod + '0');
+						
 			if (col < 9)
 			{
 				_putchar(',');
