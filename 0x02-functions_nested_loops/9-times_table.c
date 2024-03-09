a92e079b238bb816424c248f038d2908817ca282
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-03-09 17:09:17 +0000

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+/**
+  * times_table - print 9 times table
+  */
+#include "main.h"
+void times_table(void)
+{
+	int row, col, prod;
+
+	for (row = 0; row < 10; row++)
+	{
+		for (col = 0; col < 10; col++)
+		{
+			prod = row * col;
+			if (prod > 9)
+			{
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			else
+			{
+				_putchar(32);
+				_putchar(prod + '0');
+			}
+			if (col < 9)
+			{
+				_putchar(',');
+				_putchar(32);
+			}
+
+		}
+		_putchar('\n');
+	}
+}
