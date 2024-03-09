236a7923ea390033dfd9ef5b8ae48403e441d175
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-03-09 19:48:07 +0000

    remove space beyond 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,25 +6,25 @@
 #include "main.h"
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
 		while (n <= 98)
 		{
 			if (n < 98)
 				printf("%d, ", n);
 			else
-				printf("%d ", n);
+				printf("%d", n);
 			n++;
 		}
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
 		while (n >= 98)
 		{
 			if (n > 98)
 				printf("%d, ", n);
 			else
-				printf("%d ", n);
+				printf("%d", n);
 			n--;
 		}
 	}
