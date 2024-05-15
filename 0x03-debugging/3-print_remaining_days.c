2c5d10a5a3a36b31223cbd3706153a8b9b44b331
ElusiveJames <jmtokens@gmail.com>
2024-05-15 09:24:18 +0000

    stop leap

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
 * print_remaining_days - takes a date and prints how many days are
@@ -14,9 +14,9 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
-		day++;
+			day++;
 		}
 
 		printf("Day of the year: %d\n", day);
@@ -30,8 +30,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
