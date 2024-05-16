e2dea56459507899426ed8fa5b4c8ddccc6871d6
ElusiveJames <jmtokens@gmail.com>
2024-05-16 14:02:23 +0000

    fizzy bus

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,14 +9,19 @@ int main(void)
 
 	for (; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-			printf("FizzBuzz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
+		if (i < 100)
+		{
+			if (i % 15 == 0)
+				printf("FizzBuzz ");
+			else if (i % 5 == 0)
+				printf("Buzz ");
+			else if (i % 3 == 0)
+				printf("Fizz ");
+			else
+				printf("%d ", i);
+		}
 		else
-			printf("%d ", i);
+			printf("Buzz");
 	}
 	printf("\n");
 	return (0);
