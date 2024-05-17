5ff42fd524dde52c08a14939df22e7f6f2306ea4
ElusiveJames <jmtokens@gmail.com>
2024-05-17 05:07:31 +0000

    more help

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,11 @@ void more_numbers(void)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			if (i < 10)
-				_putchar(i + '0');
-			else
-			{
+			if (i > 9)
 				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
+			_putchar((i % 10) + '0');
 		}
-		_putchar('\n');
-		j++;
+	_putchar('\n');
+	j++;
 	}
 }
