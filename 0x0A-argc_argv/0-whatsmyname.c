8c95be31605d370cd9ef2dcab52c4404846dc065
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-06-06 11:31:43 +0300

    its what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - check the code
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+		printf("%s\n", argv[0]);
+	return (0);
+}
