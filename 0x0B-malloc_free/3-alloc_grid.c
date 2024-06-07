3b8ff5651a56a076f7266ebdbe8474a08e9943f5
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-06-07 15:39:53 +0300

    i will beat you

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,23 +13,19 @@ int **alloc_grid(int width, int height)
 
 	if (height <= 0 || width <= 0)
 		return (NULL);
+	/* allocating memeory for each pointer	*/
 	matrix  = malloc(sizeof(int *) * height);
 	if (matrix == NULL)
 	{
 		free(matrix);
 		return (NULL);
 	}
+	/* allocating memory for each row*/
 	for (i = 0; i < height; i++)
 	{
 		matrix[i] = malloc(width * sizeof(int));
-		if (matrix[i] == NULL)
-		{
-			for (i--; i >= 0; i--)
-				free(matrix[i]);
-			free(matrix);
-			return (NULL);
-		}
 	}
+	/*initializing the 2d array*/
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
