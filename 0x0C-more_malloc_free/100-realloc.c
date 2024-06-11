3e2e1421f132cc320785565ebf09c5796c4c9dd4
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-06-11 10:32:12 +0300

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr;
+	char *old_ptr;
+	unsigned int i;
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		return (new_ptr);
+	}
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	old_ptr = ptr;
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			new_ptr[i] = old_ptr[i];
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	free(old_ptr);
+	return (new_ptr);
+	
+}
