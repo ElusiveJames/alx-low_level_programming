9aa6bc4741c2a3a35c781255e3da56c484da4e9a
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-06-12 14:18:20 +0300

    dog best man friend

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,13 +1,24 @@
 /**
-  *print_dog - print structure variable
-  *@d: pointer to variable of struct type
-  */
+	*print_dog - print structure variable
+	*@d: pointer to variable of struct type
+	*/
 #include <stdio.h>
 #include "dog.h"
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
 
