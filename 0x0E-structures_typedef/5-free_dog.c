4faf9328db04dea2f1238f421225b04529e5d8aa
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-06-12 15:07:48 +0300

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,11 +1,14 @@
 #include "dog.h"
-
+/**
+* free_dog - function that frees dogs
+* @d: pointer to dog
+*/
 void free_dog(dog_t *d)
 {
-    if (d)
-    {
-        free(d->name);
-        free(d->owner);
-        free(d);
-    }
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
