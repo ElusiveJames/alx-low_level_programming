7bf807cbdd7ee6b5cb79863739ec25d9c0c8cd8c
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-06-13 20:13:35 +0300

    hackers are adaptable

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+/**
+* main -print opcode of itself
+* @argc: argument count
+* @argv: argument array
+*
+* Return: 1 if no argument supplied 2 if bytes to print
+* are negative 0 on success
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	void *pt_to_main;
+	unsigned char *opcode_ptr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int bytes = atoi(argv[1]);
+
+	if (bytes <= 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	pt_to_main = (void *)&main;
+	opcode_ptr = (unsigned char *)pt_to_main;
+
+	for (i = 0; i < bytes; i++)
+		printf("%02" PRIx8, opcode_ptr[i]);
+	printf("\n");
+}
